b91672e0f9521875671bd441febedde059132c8c
Achim Abeling <achimabeling@web.de>
2019-11-01 15:01:55 +0100

    added joystick control

--- src/main/java/de/banapple/drivingschool/DrivingSchool.java
+++ src/main/java/de/banapple/drivingschool/DrivingSchool.java
@@ -1,7 +1,11 @@
 package de.banapple.drivingschool;
 
+import java.util.*;
+
 import com.jme3.app.*;
 import com.jme3.asset.*;
+import com.jme3.input.*;
+import com.jme3.input.controls.*;
 import com.jme3.light.*;
 import com.jme3.material.*;
 import com.jme3.math.*;
@@ -17,30 +21,53 @@ import com.jme3.system.*;
 public class DrivingSchool extends SimpleApplication {
 
     public static void main(String[] args) {
-        
+
         AppSettings settings = new AppSettings(true);
-        settings.setResolution(1280,960);
-        
+        settings.setResolution(640, 480);
+        settings.setUseJoysticks(true);
+
         DrivingSchool app = new DrivingSchool();
         app.setSettings(settings);
         app.setShowSettings(false);
         app.start(); // start the game
     }
 
+    protected Geometry car;
+    private Joystick joystick;
+    private Vector3f drivingDirection;
+    float speed;
+    
     @Override
     public void simpleInitApp() {
 
+        Joystick[] joysticks = inputManager.getJoysticks();
+        if (joysticks == null) {
+            throw new IllegalStateException("Cannot find any joysticks!");
+        }
+        Arrays.asList(joysticks).forEach(j -> System.out.println(j));
+        if (joysticks.length != 1) {
+            throw new IllegalStateException("more than one joystick");
+        }
+        joystick = joysticks[0];
+
+        /* disable FlyCamAppState with all the key and joystick bindings */
+        stateManager.detach(stateManager.getState(FlyCamAppState.class));
+        
         Box b = new Box(1, 1, 1); // create cube shape
-        Geometry geom = new Geometry("Box", b); // create cube geometry from the
-                                                // shape
+        car = new Geometry("Car", b); // create cube geometry from the shape
         Material mat = new Material(assetManager,
                 "Common/MatDefs/Misc/Unshaded.j3md"); // create a simple
                                                       // material
         mat.setColor("Color", ColorRGBA.Blue); // set color of material to blue
-        geom.setMaterial(mat); // set the cube's material
-        rootNode.attachChild(geom); // make the cube appear in the scene
-        
+        car.setMaterial(mat); // set the cube's material
+        rootNode.attachChild(car); // make the cube appear in the scene
+
         createWorld(rootNode, assetManager);
+
+        initKeys();
+        
+        drivingDirection = new Vector3f(1, 0, 0);
+        speed = 0;
     }
 
     /**
@@ -51,7 +78,7 @@ public class DrivingSchool extends SimpleApplication {
      * @param assetManager
      */
     public void createWorld(Node rootNode, AssetManager assetManager) {
-        
+
         AmbientLight light = new AmbientLight();
         light.setColor(ColorRGBA.LightGray);
         rootNode.addLight(light);
@@ -67,31 +94,89 @@ public class DrivingSchool extends SimpleApplication {
         // plane.setOriginNormal(new Vector3f(0, 0.25f, 0), Vector3f.UNIT_Y);
         // floorGeometry.addControl(new RigidBodyControl(new
         // PlaneCollisionShape(plane), 0));
-//        floorGeometry.addControl(new RigidBodyControl(0));
+        // floorGeometry.addControl(new RigidBodyControl(0));
         rootNode.attachChild(floorGeometry);
-//        space.add(floorGeometry);
-//
-//        // movable boxes
-//        for (int i = 0; i < 12; i++) {
-//            Box box = new Box(0.25f, 0.25f, 0.25f);
-//            Geometry boxGeometry = new Geometry("Box", box);
-//            boxGeometry.setMaterial(material);
-//            boxGeometry.setLocalTranslation(i, 5, -3);
-//            // RigidBodyControl automatically uses box collision shapes when
-//            // attached to single geometry with box mesh
-//            boxGeometry.addControl(new RigidBodyControl(2));
-//            rootNode.attachChild(boxGeometry);
-//            space.add(boxGeometry);
-//        }
-//
-//        // immovable sphere with mesh collision shape
-//        Sphere sphere = new Sphere(8, 8, 1);
-//        Geometry sphereGeometry = new Geometry("Sphere", sphere);
-//        sphereGeometry.setMaterial(material);
-//        sphereGeometry.setLocalTranslation(4, -4, 2);
-//        sphereGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(sphere), 0));
-//        rootNode.attachChild(sphereGeometry);
-//        space.add(sphereGeometry);
+        // space.add(floorGeometry);
+        //
+        // // movable boxes
+        // for (int i = 0; i < 12; i++) {
+        // Box box = new Box(0.25f, 0.25f, 0.25f);
+        // Geometry boxGeometry = new Geometry("Box", box);
+        // boxGeometry.setMaterial(material);
+        // boxGeometry.setLocalTranslation(i, 5, -3);
+        // // RigidBodyControl automatically uses box collision shapes when
+        // // attached to single geometry with box mesh
+        // boxGeometry.addControl(new RigidBodyControl(2));
+        // rootNode.attachChild(boxGeometry);
+        // space.add(boxGeometry);
+        // }
+        //
+        // // immovable sphere with mesh collision shape
+        // Sphere sphere = new Sphere(8, 8, 1);
+        // Geometry sphereGeometry = new Geometry("Sphere", sphere);
+        // sphereGeometry.setMaterial(material);
+        // sphereGeometry.setLocalTranslation(4, -4, 2);
+        // sphereGeometry.addControl(new RigidBodyControl(new
+        // MeshCollisionShape(sphere), 0));
+        // rootNode.attachChild(sphereGeometry);
+        // space.add(sphereGeometry);
 
+    }
+
+    private void initKeys() {
+
+        System.out.println(inputManager);
+
+        inputManager.clearRawInputListeners();
+
+        inputManager.addMapping("Left", new JoyAxisTrigger(
+                joystick.getJoyId(),
+                joystick.getXAxisIndex(), true));
+        inputManager.addMapping("Right", new JoyAxisTrigger(
+                joystick.getJoyId(),
+                joystick.getXAxisIndex(), false));
+        inputManager.addMapping("Forward", new JoyAxisTrigger(
+                joystick.getJoyId(),
+                joystick.getYAxisIndex(), true));
+        inputManager.addMapping("Back", new JoyAxisTrigger(
+                joystick.getJoyId(),
+                joystick.getYAxisIndex(), false));
+        inputManager.addListener(analogListener, "Left", "Right", "Forward", "Back");
+
+    }
+
+    private final AnalogListener analogListener = new AnalogListener() {
+
+        @Override
+        public void onAnalog(String name, float value, float tpf) {
+
+            speed = 0;            
+            
+            if (name.equals("Right")) {
+                car.rotate(0, -value, 0);
+                drivingDirection = car.getLocalTransform().getRotation().getRotationColumn(2);
+            }
+            if (name.equals("Left")) {
+                car.rotate(0, value, 0);
+                drivingDirection = car.getLocalTransform().getRotation().getRotationColumn(2);
+            }
+
+            if ("Forward".equals(name)) {
+                speed = 1000 * value * tpf;
+            }
+
+            if ("Back".equals(name)) {
+                speed = -1000 * value * tpf;
+            }
+            
+            System.out.println(name + " " + value + " " + tpf + " speed: " + speed
+                    + " drivingDirection: " + drivingDirection);
+        }
+    };
+
+    @Override
+    public void simpleUpdate(float tpf) {
+
+        car.move(drivingDirection.mult(speed));
     }
 }
